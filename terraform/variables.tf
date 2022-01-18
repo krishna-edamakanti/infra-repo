0af2f07dc801ae285a65294a61268803f3035f19
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-18 15:01:13 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "rgname" {
+  description = "Resource Group Name"
+  default     = "ice_rg"
+  type        = string
+}
+variable "location" {
+  description = "Azure location"
+  default     = "East US"
+  type        = string
+}
+variable "sname" {
+  description = "Azure Storage Account"
+  type        = string
+}
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+}
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+}
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+variable "node_resource_group" {
+  type        = string
+  description = "RG name for cluster resources in Azure"
+}
