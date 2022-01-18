e1acdfede5688e87eda863227ec928654b92d11d
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-18 14:58:40 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+# File test
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDevOps"
+    storage_account_name = "fiservdevopssa"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+module "RG" {
+  source   = "./modules/RG" #A
+  rgname   = var.rgname     #B
+  location = var.location
+}
+module "SA" {
+  source   = "./modules/StorageAccount"
+  sname    = var.sname
+  rgname   = var.rgname
+  location = var.location
+}
+module "AKS" {
+  source              = "./modules/AKS"
+  cluster_name        = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  rgname              = var.rgname
+  node_resource_group = var.node_resource_group
+  system_node_count   = var.system_node_count
+}
